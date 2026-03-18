0e6479ef42f1507afa85d1a3c35e257e1cbae69d
jianli <Suri-Li@hotmail.com>
2026-03-18 17:23:52 -0400

    added code

--- columns.asm
+++ columns.asm
@@ -179,6 +179,155 @@ syscall
 gem_landed:
 jal store_gems_in_grid
 
+match_loop:
+jal check_matches
+jal clear_matches
+beq $v0, $zero, no_more_matches
+jal drop_gems
+j match_loop
+
+no_more_matches:
+jal draw_new_gem
+j game_loop
+
+# Functions
+clear_grid_memory:
+la $t0, grid
+li $t1, 0
+li $t2 4096
+
+clear_grid_loop:
+bge $t1, $t2, clear_other_start
+sw $zero, 0($t0)
+addi $t0, $t0, 4
+addi $t1, $t1, 4
+j clear_grid_loop
+
+clear_other_start:
+la $t0, other_grid
+li $t1, 0
+
+clear_other_loop:
+bge $t1, $t2, clear_grid_done
+sw $zero, 0($t0)
+addi $t0, $t0, 4
+addi $t1, $t1, 4
+j clear_other_loop
+
+clear_grid_done:
+jr $ra
+
+# Randomly generate a color for 1 gem
+gem_color:
+li $v0, 42
+li $a0, 0
+li $a1, 6
+syscall
+la $t4, colors
+sll $t5, $a0, 2
+addu $t4, $t4, $t5
+lw, $t1, 0($t4)
+jr $ra
+
+# Spawn a new vertical 3-gem column at top-middle
+draw_new_gem:
+addi $sp, $sp, -4
+sw $ra, 0($sp)
+la $t6, gem_colors
+jal gem_color
+sw $t1, 0($t6)
+jal gem_color
+sw $t1, 4($t6)
+jal gem_color
+sw $t1, 8($t6)
+lw $t0, ADDR_DSPL
+addi $t7, $t0, 152
+lw $ra, 0($sp)
+addi $sp, $sp, 4
+jr $ra
+
+draw_gem:
+la $t6, gem_colors
+lw $t1, 0($t6)
+sw $t1, 0($t7)
+lw $t1, 4($t6)
+sw $t1, 128($t7)
+lw $t1, 8($t6)
+sw $t1, 256($t7)
+jr $ra
+
+# Erase current vertical falling column
+delete_gem:
+li $t1, 0x000000
+sw $t1, 0($t7)
+sw $t1, 128($t7)
+sw $t1, 256($t7)
+jr $ra
+
+# Store landed vertical column into grid memory
+store_gems_in_grid:
+addi $sp, $sp, -8
+sw $ra, 0($sp)
+sw $t0, 4($sp)
+
+la $t6, gem_colors
+lw $t0, ADDR_DSPL
+sub $t2, $t7, $t0
+la $t3, grid
+add $t3, $t3, $t2
+
+lw $t1, 0($t6)
+sw $t1, 0($t3)
+
+lw $t1, 4($t6)
+sw $t1, 128($t3)
+
+lw $t1, 8($t6)
+sw $t1, 256($t3)
+
+lw $ra, 0($sp)
+lw $t0, 4($sp)
+addi $sp, $sp, 8
+jr $ra
+
+# Game over
+check_col_full:
+# input a0 = playable column number 4..9
+lw $t0, ADDR_DSPL
+sll $t1, $a0, 2
+add $t0, $t0, $t1
+addi $t0, $t0, 128   # row 1
+
+move $a0, $t0
+jal get_color_grid
+beq $v0, $zero, col_not_full
+
+li $v0, 1
+jr $ra
+
+col_not_full:
+li $v0, 0
+jr $ra
+
+check_any_col_full:
+li $t0, 4
+
+check_any_col_loop:
+bgt $t0, 9, no_col_full
+move $a0, $t0
+jal check_col_full
+bne $v0, $zero, some_col_full
+addi $t0, $t0, 1
+j check_any_col_loop
+
+some_col_full:
+li $v0, 1
+jr $ra
+
+no_col_full:
+li $v0, 0
+jr $ra
+    
 draw_hor_line:
 lw $t0, ADDR_DSPL
 sll $a0, $a0, 2
